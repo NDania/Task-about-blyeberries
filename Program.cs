ef3195a4de852c33f6a6191f106e02ef02a4d056
NDania <113174899+NDania@users.noreply.github.com>
2022-09-24 17:59:25 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿//Дополнительная задача по сбору черники
+Console.Clear();
+Console.WriteLine("Введите количество кустов от 3 до 1000: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int [] a = new int[n];
+
+if (n < 3 || n > 1000)
+    Console.WriteLine("Вы ошиблись при вводе и не попали в указанный диапазон. Повторите попытку.");
+else
+{
+    for(int i = 0; i < n; i++)
+        a[i] = i+1;
+               
+ int sum = a[n-1] + a[n-2] + a[n-3];  
+ Console.WriteLine("Сумма макс.кол-ва собранных ягод за один заход = " + sum);
+}
